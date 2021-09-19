8644085796e822e4b2275ce539749710f222e439
Kiaev Ivan <dapa_maru@mail.ru>
2021-09-19 08:32:10 +0600

    according to issue "Test names #7" amend class ReverserTest: names methods

--- src/test/java/ReverserTest.java
+++ src/test/java/ReverserTest.java
@@ -3,71 +3,57 @@ import static org.junit.Assert.*;
 import org.junit.Ignore;
 
 public class ReverserTest {
-    
     @Test
-    public void testReverse() {
+    public void reverse_ShouldReversWholeWord_ifOneWordAndOnlyCapitalLetters() {
         String str = "ABC";
         String ans = "CBA";
         String expect = ans;
         String result = Reverser.reverseWithoutSpecialSymbols(str);
         assertEquals(expect,result);
-        System.out.println("test 'Reverse Alpha versions' is worked succesful");
     }
 
     @Test
-    public void testEmpty() {
+    public void empty_ShouldThrowAnExceptionIfStringEmpty() {
         String str = "ABC";
         String ans = "";
         String expect = ans;
         String result = Reverser.reverseWithoutSpecialSymbols(str);
         assertNotEquals(expect,result);
-        System.out.println("test 'Is not Empty', succesful");
     }
 
     @Test
-    public void testNULL() {
+    public void null_ShouldThrowAnExceptionIfStringNull() {
         String str = "ABC";
         String ans = null;
         String expect = ans;
         String result = Reverser.reverseWithoutSpecialSymbols(str);
         assertNotEquals(expect,result);
-        System.out.println("test 'Is not NULL', succesful");
     }
 
     @Test
-    public void testReverseSimple() {
+    public void simple_ShouldReversWholeWordsIfWordsInLowCaseAndWithoutSpecialSymbols() {
         String str = "abcd efg hijk";
         String ans =  "dcba gfe kjih";
         String expect = ans;
         String result = Reverser.reverseWithoutSpecialSymbols(str);
         assertEquals(expect,result);
-        System.out.println("test 'ReverseSimple' is worked succesful");
     }
 
     @Test
-    public void testReverseSpecialSymbols() {
+    public void difficultOne_ShouldReversWholeWord_IfOnlyOneWordWithSpicialSymbolsAndNummerics() {
         String str = "a1bc!de2f@gh";
         String ans = "h1gf!ed2c@ba";
         String expect = ans;
         String result = Reverser.reverseWithoutSpecialSymbols(str);
         assertEquals(expect,result);
-        System.out.println("test 'ReverseSpecialSymbols' is worked succesful");
     }
 
     @Test
-    public void testReverseSpecialSymbolsAndNumerics() {
+    public void difficultSeveral_ShouldReversWholeWords_IfSeveralWordsWithSpicialSymbolsAndNummerics() {
         String str = "a1Bc!D e2f@G hI3jK#";
         String ans =  "D1cB!a G2f@e Kj3Ih#";
         String expect = ans;
         String result = Reverser.reverseWithoutSpecialSymbols(str);
         assertEquals(expect,result);
-        System.out.println("test 'ReverseSpecialSymbolsAndNumerics' is worked succesful");
     }
-
-    @Ignore
-    public void testNullPointerException(){
-        String str = null;
-        String result = Reverser.reverseWithoutSpecialSymbols(str);
-    }
-
 }
