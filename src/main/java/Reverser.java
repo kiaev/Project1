8775d0ebb3ea6959d7e2668ce28a860579b416c6
Kiaev Ivan <dapa_maru@mail.ru>
2021-09-21 22:55:17 +0600

    Return back to one commit

--- src/main/java/Reverser.java
+++ src/main/java/Reverser.java
@@ -4,42 +4,47 @@ public class Reverser {
 
     public static String reverseOnlyWords (String str){
         String[] words = checkNull(str).split(" ");
-        return reverseOneWord(words);
+        StringJoiner message = new StringJoiner(" ");
+
+        for (int i = 0; i <= words.length - 1; i ++){
+            char[] array = words[i].toCharArray();
+            // Реверс символьного массива, преобразование к строке
+            message.add (String.valueOf(Reverser.reverseOneWord(array)));
+        }
+        return message.toString();
     }
 
     private static String checkNull (String str) {
         String checkvalue = str;
+
         if  (checkvalue == null){
             throw new IllegalArgumentException("The string value must not be null");
         }
         return checkvalue;
     }
 
-    private static String reverseOneWord (String[] words)  {
-        StringJoiner message = new StringJoiner(" ");
-        for (int i = 0; i <= words.length - 1; i ++) {
-            char[] array = words[i].toCharArray();
-            // Инициализируем левый и правый указатели Обход строки с обоих концов до 'l' и 'r'
-            int r = array.length - 1;
-            int l = 0;
-            while (l < r) {
-                // Игнорировать специальные символы
-                if (!Character.isAlphabetic(array[l])) {
-                    l++;
-                } else if (!Character.isAlphabetic(array[r])) {
-                    r--;
-                }
-                // И str [l], и str [r] не являются пространственными
-                else {
-                    char tmp = array[l];
-                    array[l] = array[r];
-                    array[r] = tmp;
-                    l++;
-                    r--;
-                }
+    private static char[] reverseOneWord (char[] str)  {
+        // Инициализируем левый и правый указатели Обход строки с обоих концов до 'l' и 'r'
+        int r = str.length - 1;
+        int l = 0;
+
+        while (l < r) {
+            // Игнорировать специальные символы
+            if (!Character.isAlphabetic(str[l])) {
+                l++;
             }
-            message.add (String.valueOf(array));
+            else if(!Character.isAlphabetic(str[r])) {
+                r--;
+            }
+                // И str [l], и str [r] не являются пространственными
+            else {
+                char tmp = str[l];
+                str[l] = str[r];
+                str[r] = tmp;
+                l++;
+                r--;
+            }
         }
-        return message.toString();
+        return str;
     }
 }
