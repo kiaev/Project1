610d2471e1301622eb562bd847fe63702fb786f3
Kiaev Ivan <dapa_maru@mail.ru>
2021-09-21 21:20:24 +0600

    Change Class Reverser according LSP change methods:reverseOnlyWords, reverseOneWord

--- src/main/java/Reverser.java
+++ src/main/java/Reverser.java
@@ -4,47 +4,42 @@ public class Reverser {
 
     public static String reverseOnlyWords (String str){
         String[] words = checkNull(str).split(" ");
-        StringJoiner message = new StringJoiner(" ");
-
-        for (int i = 0; i <= words.length - 1; i ++){
-            char[] array = words[i].toCharArray();
-            // Реверс символьного массива, преобразование к строке
-            message.add (String.valueOf(Reverser.reverseOneWord(array)));
-        }
-        return message.toString();
+        return reverseOneWord(words);
     }
 
     private static String checkNull (String str) {
         String checkvalue = str;
-
         if  (checkvalue == null){
             throw new IllegalArgumentException("The string value must not be null");
         }
         return checkvalue;
     }
 
-    private static char[] reverseOneWord (char[] str)  {
-        // Инициализируем левый и правый указатели Обход строки с обоих концов до 'l' и 'r'
-        int r = str.length - 1;
-        int l = 0;
-
-        while (l < r) {
-            // Игнорировать специальные символы
-            if (!Character.isAlphabetic(str[l])) {
-                l++;
-            }
-            else if(!Character.isAlphabetic(str[r])) {
-                r--;
-            }
+    private static String reverseOneWord (String[] words)  {
+        StringJoiner message = new StringJoiner(" ");
+        for (int i = 0; i <= words.length - 1; i ++) {
+            char[] array = words[i].toCharArray();
+            // Инициализируем левый и правый указатели Обход строки с обоих концов до 'l' и 'r'
+            int r = array.length - 1;
+            int l = 0;
+            while (l < r) {
+                // Игнорировать специальные символы
+                if (!Character.isAlphabetic(array[l])) {
+                    l++;
+                } else if (!Character.isAlphabetic(array[r])) {
+                    r--;
+                }
                 // И str [l], и str [r] не являются пространственными
-            else {
-                char tmp = str[l];
-                str[l] = str[r];
-                str[r] = tmp;
-                l++;
-                r--;
+                else {
+                    char tmp = array[l];
+                    array[l] = array[r];
+                    array[r] = tmp;
+                    l++;
+                    r--;
+                }
             }
+            message.add (String.valueOf(array));
         }
-        return str;
+        return message.toString();
     }
 }
